73ecc6806c679846527a470c03189336eeba66cb
weejo <wendel.jo@hotmail.com>
2024-01-27 07:59:58 +0100

    Added GameOverScene

---
+++ src/constants/urls.ts
@@ -0,0 +1,6 @@
+export const URLS = {
+    LEVELDATA: 'http://localhost:8080/level?levelId=',
+    HIGHSCORE: 'http://localhost:8080/highscore?levelId=',
+    ADDRESULT:'http://localhost:8080/result',
+    OVERVIEW:'http://localhost:8080/overview'
+}
--- src/scenes/GameOverScene.ts
+++ src/scenes/GameOverScene.ts
@@ -1,11 +1,20 @@
 import {SCENES} from "../constants/scenes.ts";
 import {REGISTRY} from "../constants/registry.ts";
+import {URLS} from "../constants/urls.ts";
 
 export class GameOverScene extends Phaser.Scene {
+    levelId: number;
+
     constructor() {
         super({
             key: SCENES.GAMEOVER, active: false
         });
+        this.levelId = 61;
+    }
+
+    init(data: any) {
+        // TODO: USE NONE STATIC LEVELID
+        //this.levelId = data.levelId;
     }
 
 
@@ -19,5 +28,23 @@ export class GameOverScene extends Phaser.Scene {
         this.add.text(width / 2, height / 2, "Points: " + this.registry.get(REGISTRY.SCORE).toFixed(0).toString())
             .setScale(5)
             .setColor('#FFFFFF');
+
+        this.publishData();
+    }
+
+    private publishData() {
+        fetch(URLS.ADDRESULT, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "entry": null,// TODO: ADD REAL PLAYER DATA HERE
+                "levelId": this.levelId,
+                "data": this.registry.get(REGISTRY.CLUSTER)
+            })
+
+        }).then();
     }
 }
